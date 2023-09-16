d40335d2db7e3754eb1d746bd081b62fff728bbf
Krantuckety <nferenczhalmy@gmail.com>
2023-09-15 22:56:39 -0500

    Added more test matrixes, added comments

--- VerticalMatrixProject/main.cpp
+++ VerticalMatrixProject/main.cpp
@@ -1,4 +1,13 @@
-// Put in a prologue
+/*
+Program Name: Vertical Matrix Project
+Date: 9/15/2023
+Author: Noah S Ferenczhalmy
+Module Purpose
+This program reads matrixes from a file and creates a matrix in the program from what it reads. 
+The program then displays the matrixes it read as well as the sums of all columns within the matrix and checks whether the columns possess vertical symmetry.
+The program will then sort each row in ascending order from left to right. 
+After all functions are completed, it will prompt the user have it read the next matrix and perform the same functions until all matrixes within the file have been read.
+*/
 
 #include <string>
 #include <iostream>
@@ -7,6 +16,7 @@
 
 using namespace std;
 
+// Constant containing the maximum size for a Matrix.
 const unsigned MAX_MATRIX_SIZE   = 20;
 const unsigned WIDTH_DISPLAY     = 12;
 
@@ -53,6 +63,9 @@ int main() {
    exit(EXIT_SUCCESS);
 }
 
+// The readMatrixFromFile method checks if there is a valid height and width inputed in the file it is reading from.
+// If the height and width are valid, then a nested for loop runs through as many columns as the "height" variable says there are and as many rows as the "width" variable says there is.
+// All values are then stored in the same order then were in the original file inside of a matrix.
 bool readMatrixFromFile(int matrix[][MAX_MATRIX_SIZE], unsigned& height, unsigned& width, ifstream& inputFileStreamObj) 
 {
     if (!(inputFileStreamObj >> height))
@@ -65,7 +78,7 @@ bool readMatrixFromFile(int matrix[][MAX_MATRIX_SIZE], unsigned& height, unsigne
         return(false);
 }
 
-
+// The displayMatrix method uses a nested for loop to go through every value in an matrix that fits between the given height and width values. Then, it display them as they were in the matrixes.txt file
 void displayMatrix(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width) {
     for (unsigned row = 0; row < height; row++)
     {
@@ -78,7 +91,8 @@ void displayMatrix(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned widt
     cout << endl;
 }
 
-
+// The displayCalcMatrixSums method uses a nested for loop to filter through every row that has a value in a column. 
+// Once it has gotten the sum of all values in the column, it will output the sum and move to the next column until all columns sums have been added together.
 void displayCalcMatrixSums(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsigned width, int colSums[MAX_MATRIX_SIZE]) 
 {
     for (unsigned col = 0; col < width; col++)
@@ -86,16 +100,18 @@ void displayCalcMatrixSums(int matrix[][MAX_MATRIX_SIZE], unsigned height, unsig
         colSums[col] = 0;
         for (unsigned row = 0; row < height; row++)
         {
+            // It will collect the value of each row in the column and add them together.
             colSums[col] += matrix[row][col];
             
         }
         cout << setw(WIDTH_DISPLAY) << colSums[col];
-        //cout << "\n";
     }
     cout << endl;
  }
  
-
+// The symetryCheckMatrix method uses a for loop to compare columns on opposite ends of the matrix that are the same distance away from the minimum and maximum width respectively.
+// The local variable "symmetryCheck" stores a boolean value for if 2 columns are vertically symmetric. If it ever outputs false, the for loop is exited.
+// If the for loop exits at the original condition of "posS <= posE", then the matrix is vertically symmetric. If it exits earlier, it is declared not vertically symmetric.
 void symmetryCheckMatrix(int colSums[], unsigned width, unsigned height) {
     bool symmetryCheck = false;
     for (unsigned posS = 0, posE = width - 1; posS <= posE; posS++, posE--)
@@ -110,6 +126,10 @@ void symmetryCheckMatrix(int colSums[], unsigned width, unsigned height) {
         cout << "\nThis matrix is not Vertically Symmetric.\n\n";
 }
 
+// The sortMatrixRows method used a for loop to filter through every value in a matrix.
+// Within each row, it compares a value starting from the value in position 1 to the value before it. 
+// If the previous value is greater than the current value being examined, then they are swapped and the row is checked from the position at value 1 again.
+// The method ends once all rows have been sorted.
 void sortMatrixRows(int matrix[][MAX_MATRIX_SIZE], unsigned width, unsigned height) {
     unsigned colStore;
     for (unsigned row = 0; row < height; row++)
